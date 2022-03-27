c52b1dd4f5097566be44240bdd347c437b8941e5
guansoonpeh <pehgs@tarc.edu.my>
2022-03-27 09:09:48 +0800

    init

--- app/src/main/java/com/example/firestoragedemo/MainActivity.kt
+++ app/src/main/java/com/example/firestoragedemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.firestoragedemo
 import android.app.Activity
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,24 +12,39 @@ import com.example.firestoragedemo.databinding.ActivityMainBinding
 import com.google.firebase.storage.FirebaseStorage
 //import com.google.firebase.storage.FirebaseStorage
 import java.io.File
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var imgUri : Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         val ref = FirebaseStorage.getInstance().reference
 
         binding.btnBrowse.setOnClickListener(){
 
             startForResult.launch("image/*")
+
         }
 
 
         binding.btnUpload.setOnClickListener(){
+            val imgRef = ref.child("a.png")
+
+            imgRef.putFile(imgUri)
+                .addOnSuccessListener {
+                    binding.imageView.setImageURI(null)
+
+                }
+                .addOnFailureListener(){ e->
+                    Toast.makeText(this, e.message,  Toast.LENGTH_SHORT).show()
+                }
 
         }
 
@@ -48,7 +64,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     val startForResult = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        imgUri = uri
         binding.imageView.setImageURI(uri)
+
     }
 
 }
